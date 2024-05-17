b0073947f2b31320133a83d163616abb8f8388fe
Kanat <k.esenamanov@smfox.ri>
2024-05-17 15:07:41 +0300

    screens routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/data/mock_data.dart';
 import 'package:meals/screens/categories.dart';
-import 'package:meals/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -25,7 +22,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const MealsScreen(meals: dummyMeals, title: 'Some dummy data',),
+      home: const CategoriesScreen(),
     );
   }
 }
